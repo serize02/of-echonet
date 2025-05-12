99ceab1a0d4996ac025e5fc35e572d92974e83e5
Ernesto Serize <ernestoserize@gmail.com>
2025-05-12 02:08:27 -0400

    feat: 00_eda

--- scripts/masks.py
+++ scripts/masks.py
@@ -3,20 +3,20 @@ import cv2
 import numpy as np
 import pandas as pd
 
-meta = pd.read_csv('data/raw/A4C/VolumeTracings_new.csv')
-split = pd.read_csv('data/raw/A4C/FileList_new.csv')
+meta = pd.read_csv('data/echonet/A4C/VolumeTracings_new.csv')
+split = pd.read_csv('data/echonet/A4C/FileList_new.csv')
 
 grouped = meta.groupby(['FileName', 'Frame'])
 
 sp = ['train', 'val', 'test']
 
 for s in sp:
-    os.makedirs(f'data/processed/{s}/images', exist_ok=True)
-    os.makedirs(f'data/processed/{s}/masks', exist_ok=True)
+    os.makedirs(f'data/raw/{s}/images', exist_ok=True)
+    os.makedirs(f'data/raw/{s}/masks', exist_ok=True)
 
 for (filename, frame_num), group in grouped:
 
-    video_path = f'data/raw/A4C/videos/{filename}'
+    video_path = f'data/echonet/A4C/videos/{filename}'
     cap = cv2.VideoCapture(video_path)
 
     cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num-1)
@@ -30,7 +30,7 @@ for (filename, frame_num), group in grouped:
     sp = str.lower(split[split['FileName'] == filename[:-4]]['Split'].values[0])
 
 
-    image_path = f'data/processed/{sp}/images/{filename}_frame{frame_num}.png'
+    image_path = f'data/raw/{sp}/images/{filename}_frame{frame_num}.png'
 
     cv2.imwrite(image_path, frame)
 
@@ -38,7 +38,7 @@ for (filename, frame_num), group in grouped:
     points = group[["X", "Y"]].values.astype(int)
     cv2.fillPoly(mask, [points], color=255)
     
-    mask_path = f'data/processed/{sp}/masks/{filename}_frame{frame_num}_mask.png'
+    mask_path = f'data/raw/{sp}/masks/{filename}_frame{frame_num}.png'
     cv2.imwrite(mask_path, mask)
     
     cap.release()
